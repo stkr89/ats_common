4a114f885f7d0501afcd7c2a55b500f87cc1b7ac
stkr89 <sumit.tokkar@gmail.com>
2022-06-15 00:04:12 -0500

    updated role svc name

--- pb/rolesvc/v1/rolesvc_grpc.pb.go
+++ pb/rolesvc/v1/rolesvc_grpc.pb.go
@@ -18,10 +18,10 @@ import (
 // Requires gRPC-Go v1.32.0 or later.
 const _ = grpc.SupportPackageIsVersion7
 
-// ModelSvcClient is the client API for ModelSvc service.
+// RoleSvcClient is the client API for RoleSvc service.
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
-type ModelSvcClient interface {
+type RoleSvcClient interface {
 	Create(ctx context.Context, in *CreateRequest, opts ...grpc.CallOption) (*CreateResponse, error)
 	Get(ctx context.Context, in *GetRequest, opts ...grpc.CallOption) (*GetResponse, error)
 	List(ctx context.Context, in *EmptyRequest, opts ...grpc.CallOption) (*ListResponse, error)
@@ -29,219 +29,219 @@ type ModelSvcClient interface {
 	Delete(ctx context.Context, in *DeleteRequest, opts ...grpc.CallOption) (*EmptyResponse, error)
 }
 
-type modelSvcClient struct {
+type roleSvcClient struct {
 	cc grpc.ClientConnInterface
 }
 
-func NewModelSvcClient(cc grpc.ClientConnInterface) ModelSvcClient {
-	return &modelSvcClient{cc}
+func NewRoleSvcClient(cc grpc.ClientConnInterface) RoleSvcClient {
+	return &roleSvcClient{cc}
 }
 
-func (c *modelSvcClient) Create(ctx context.Context, in *CreateRequest, opts ...grpc.CallOption) (*CreateResponse, error) {
+func (c *roleSvcClient) Create(ctx context.Context, in *CreateRequest, opts ...grpc.CallOption) (*CreateResponse, error) {
 	out := new(CreateResponse)
-	err := c.cc.Invoke(ctx, "/pb.rolesvc.v1.ModelSvc/Create", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/pb.rolesvc.v1.RoleSvc/Create", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *modelSvcClient) Get(ctx context.Context, in *GetRequest, opts ...grpc.CallOption) (*GetResponse, error) {
+func (c *roleSvcClient) Get(ctx context.Context, in *GetRequest, opts ...grpc.CallOption) (*GetResponse, error) {
 	out := new(GetResponse)
-	err := c.cc.Invoke(ctx, "/pb.rolesvc.v1.ModelSvc/Get", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/pb.rolesvc.v1.RoleSvc/Get", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *modelSvcClient) List(ctx context.Context, in *EmptyRequest, opts ...grpc.CallOption) (*ListResponse, error) {
+func (c *roleSvcClient) List(ctx context.Context, in *EmptyRequest, opts ...grpc.CallOption) (*ListResponse, error) {
 	out := new(ListResponse)
-	err := c.cc.Invoke(ctx, "/pb.rolesvc.v1.ModelSvc/List", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/pb.rolesvc.v1.RoleSvc/List", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *modelSvcClient) Update(ctx context.Context, in *UpdateRequest, opts ...grpc.CallOption) (*UpdateResponse, error) {
+func (c *roleSvcClient) Update(ctx context.Context, in *UpdateRequest, opts ...grpc.CallOption) (*UpdateResponse, error) {
 	out := new(UpdateResponse)
-	err := c.cc.Invoke(ctx, "/pb.rolesvc.v1.ModelSvc/Update", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/pb.rolesvc.v1.RoleSvc/Update", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *modelSvcClient) Delete(ctx context.Context, in *DeleteRequest, opts ...grpc.CallOption) (*EmptyResponse, error) {
+func (c *roleSvcClient) Delete(ctx context.Context, in *DeleteRequest, opts ...grpc.CallOption) (*EmptyResponse, error) {
 	out := new(EmptyResponse)
-	err := c.cc.Invoke(ctx, "/pb.rolesvc.v1.ModelSvc/Delete", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/pb.rolesvc.v1.RoleSvc/Delete", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-// ModelSvcServer is the server API for ModelSvc service.
-// All implementations must embed UnimplementedModelSvcServer
+// RoleSvcServer is the server API for RoleSvc service.
+// All implementations must embed UnimplementedRoleSvcServer
 // for forward compatibility
-type ModelSvcServer interface {
+type RoleSvcServer interface {
 	Create(context.Context, *CreateRequest) (*CreateResponse, error)
 	Get(context.Context, *GetRequest) (*GetResponse, error)
 	List(context.Context, *EmptyRequest) (*ListResponse, error)
 	Update(context.Context, *UpdateRequest) (*UpdateResponse, error)
 	Delete(context.Context, *DeleteRequest) (*EmptyResponse, error)
-	mustEmbedUnimplementedModelSvcServer()
+	mustEmbedUnimplementedRoleSvcServer()
 }
 
-// UnimplementedModelSvcServer must be embedded to have forward compatible implementations.
-type UnimplementedModelSvcServer struct {
+// UnimplementedRoleSvcServer must be embedded to have forward compatible implementations.
+type UnimplementedRoleSvcServer struct {
 }
 
-func (UnimplementedModelSvcServer) Create(context.Context, *CreateRequest) (*CreateResponse, error) {
+func (UnimplementedRoleSvcServer) Create(context.Context, *CreateRequest) (*CreateResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Create not implemented")
 }
-func (UnimplementedModelSvcServer) Get(context.Context, *GetRequest) (*GetResponse, error) {
+func (UnimplementedRoleSvcServer) Get(context.Context, *GetRequest) (*GetResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Get not implemented")
 }
-func (UnimplementedModelSvcServer) List(context.Context, *EmptyRequest) (*ListResponse, error) {
+func (UnimplementedRoleSvcServer) List(context.Context, *EmptyRequest) (*ListResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method List not implemented")
 }
-func (UnimplementedModelSvcServer) Update(context.Context, *UpdateRequest) (*UpdateResponse, error) {
+func (UnimplementedRoleSvcServer) Update(context.Context, *UpdateRequest) (*UpdateResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Update not implemented")
 }
-func (UnimplementedModelSvcServer) Delete(context.Context, *DeleteRequest) (*EmptyResponse, error) {
+func (UnimplementedRoleSvcServer) Delete(context.Context, *DeleteRequest) (*EmptyResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Delete not implemented")
 }
-func (UnimplementedModelSvcServer) mustEmbedUnimplementedModelSvcServer() {}
+func (UnimplementedRoleSvcServer) mustEmbedUnimplementedRoleSvcServer() {}
 
-// UnsafeModelSvcServer may be embedded to opt out of forward compatibility for this service.
-// Use of this interface is not recommended, as added methods to ModelSvcServer will
+// UnsafeRoleSvcServer may be embedded to opt out of forward compatibility for this service.
+// Use of this interface is not recommended, as added methods to RoleSvcServer will
 // result in compilation errors.
-type UnsafeModelSvcServer interface {
-	mustEmbedUnimplementedModelSvcServer()
+type UnsafeRoleSvcServer interface {
+	mustEmbedUnimplementedRoleSvcServer()
 }
 
-func RegisterModelSvcServer(s grpc.ServiceRegistrar, srv ModelSvcServer) {
-	s.RegisterService(&ModelSvc_ServiceDesc, srv)
+func RegisterRoleSvcServer(s grpc.ServiceRegistrar, srv RoleSvcServer) {
+	s.RegisterService(&RoleSvc_ServiceDesc, srv)
 }
 
-func _ModelSvc_Create_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _RoleSvc_Create_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(CreateRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(ModelSvcServer).Create(ctx, in)
+		return srv.(RoleSvcServer).Create(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/pb.rolesvc.v1.ModelSvc/Create",
+		FullMethod: "/pb.rolesvc.v1.RoleSvc/Create",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(ModelSvcServer).Create(ctx, req.(*CreateRequest))
+		return srv.(RoleSvcServer).Create(ctx, req.(*CreateRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _ModelSvc_Get_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _RoleSvc_Get_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(GetRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(ModelSvcServer).Get(ctx, in)
+		return srv.(RoleSvcServer).Get(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/pb.rolesvc.v1.ModelSvc/Get",
+		FullMethod: "/pb.rolesvc.v1.RoleSvc/Get",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(ModelSvcServer).Get(ctx, req.(*GetRequest))
+		return srv.(RoleSvcServer).Get(ctx, req.(*GetRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _ModelSvc_List_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _RoleSvc_List_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(EmptyRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(ModelSvcServer).List(ctx, in)
+		return srv.(RoleSvcServer).List(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/pb.rolesvc.v1.ModelSvc/List",
+		FullMethod: "/pb.rolesvc.v1.RoleSvc/List",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(ModelSvcServer).List(ctx, req.(*EmptyRequest))
+		return srv.(RoleSvcServer).List(ctx, req.(*EmptyRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _ModelSvc_Update_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _RoleSvc_Update_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(UpdateRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(ModelSvcServer).Update(ctx, in)
+		return srv.(RoleSvcServer).Update(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/pb.rolesvc.v1.ModelSvc/Update",
+		FullMethod: "/pb.rolesvc.v1.RoleSvc/Update",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(ModelSvcServer).Update(ctx, req.(*UpdateRequest))
+		return srv.(RoleSvcServer).Update(ctx, req.(*UpdateRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _ModelSvc_Delete_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _RoleSvc_Delete_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(DeleteRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(ModelSvcServer).Delete(ctx, in)
+		return srv.(RoleSvcServer).Delete(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/pb.rolesvc.v1.ModelSvc/Delete",
+		FullMethod: "/pb.rolesvc.v1.RoleSvc/Delete",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(ModelSvcServer).Delete(ctx, req.(*DeleteRequest))
+		return srv.(RoleSvcServer).Delete(ctx, req.(*DeleteRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-// ModelSvc_ServiceDesc is the grpc.ServiceDesc for ModelSvc service.
+// RoleSvc_ServiceDesc is the grpc.ServiceDesc for RoleSvc service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
-var ModelSvc_ServiceDesc = grpc.ServiceDesc{
-	ServiceName: "pb.rolesvc.v1.ModelSvc",
-	HandlerType: (*ModelSvcServer)(nil),
+var RoleSvc_ServiceDesc = grpc.ServiceDesc{
+	ServiceName: "pb.rolesvc.v1.RoleSvc",
+	HandlerType: (*RoleSvcServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
 			MethodName: "Create",
-			Handler:    _ModelSvc_Create_Handler,
+			Handler:    _RoleSvc_Create_Handler,
 		},
 		{
 			MethodName: "Get",
-			Handler:    _ModelSvc_Get_Handler,
+			Handler:    _RoleSvc_Get_Handler,
 		},
 		{
 			MethodName: "List",
-			Handler:    _ModelSvc_List_Handler,
+			Handler:    _RoleSvc_List_Handler,
 		},
 		{
 			MethodName: "Update",
-			Handler:    _ModelSvc_Update_Handler,
+			Handler:    _RoleSvc_Update_Handler,
 		},
 		{
 			MethodName: "Delete",
-			Handler:    _ModelSvc_Delete_Handler,
+			Handler:    _RoleSvc_Delete_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
