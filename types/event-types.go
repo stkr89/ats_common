e9dd25f267d1504a3f6074dbf5efefd5b76e36c5
stkr89 <sumit.tokkar@gmail.com>
2022-06-14 19:23:32 -0500

    set admin id as uuid

--- types/event-types.go
+++ types/event-types.go
@@ -4,5 +4,5 @@ import "github.com/google/uuid"
 
 type OrganizationCreatedEvent struct {
 	OrganizationID uuid.UUID `json:"organizationID" validate:"required"`
-	AdminID        string    `json:"adminID" validate:"required"`
+	AdminID        uuid.UUID `json:"adminID" validate:"required"`
 }
