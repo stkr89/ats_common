19ecae5fd0b59c42529ba11cdb6dd8046d107fb2
stkr89 <sumit.tokkar@gmail.com>
2022-06-22 21:26:51 -0500

    added application shortlisted event type

--- types/event-types.go
+++ types/event-types.go
@@ -6,3 +6,7 @@ type OrganizationCreatedEvent struct {
 	OrganizationID uuid.UUID `json:"organizationID" validate:"required"`
 	AdminEmail     string    `json:"adminID" validate:"required"`
 }
+
+type ApplicationShortlisted struct {
+	ApplicationID uuid.UUID `json:"applicationID" validate:"required"`
+}
