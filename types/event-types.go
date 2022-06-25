80441417e302233df4adca137f7e45995d2fccef
stkr89 <sumit.tokkar@gmail.com>
2022-06-25 10:14:58 -0500

    added availability created event type

--- types/event-types.go
+++ types/event-types.go
@@ -1,6 +1,9 @@
 package types
 
-import "github.com/google/uuid"
+import (
+	"github.com/google/uuid"
+	"time"
+)
 
 type OrganizationCreatedEvent struct {
 	OrganizationID uuid.UUID `json:"organizationID" validate:"required"`
@@ -9,4 +12,12 @@ type OrganizationCreatedEvent struct {
 
 type ApplicationShortlisted struct {
 	ApplicationID uuid.UUID `json:"applicationID" validate:"required"`
+}
+
+type AvailabilityCreated struct {
+	AvailabilityID  uuid.UUID `json:"availabilityID" validate:"required"`
+	CandidateID     uuid.UUID `json:"candidateID" validate:"required"`
+	RoleID          uuid.UUID `json:"roleID" validate:"required"`
+	InterviewStepID uuid.UUID `json:"interviewStepID" validate:"required"`
+	StartTime       time.Time `json:"startTime" validate:"required"`
 }
